b99c09a739fffc030b63735fb88258d02b576c39
hashimoto takumi <takumih6013@gmail.com>
2024-05-05 12:24:54 +0900

    リマインダーの実装

--- src/app/Console/Kernel.php
+++ src/app/Console/Kernel.php
@@ -15,7 +15,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('remind_mail')->dailyAt('7:00');
     }
 
     /**
